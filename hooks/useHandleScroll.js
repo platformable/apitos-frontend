e1fb4640820f7a6fb7a9626668192ce14ce87b27
leoncan122 <leon.can122@gmail.com>
2023-04-26 12:17:47 +0200

    delete file not used

--- hooks/useHandleScroll.js
+++
@@ -1,9 +0,0 @@
-export default function useHandleScroll() {
-  return (e) => {
-    e.preventDefault();
-    // console.warn(e.target.getAttribute("data-next-target"))
-    const target = e.target.getAttribute("data-next-target")
-    const location = document.querySelector(target).offsetTop
-    console.log('location',location);
-  };
-}
