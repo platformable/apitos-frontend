ef64bb64509e5161b73cb8faa6905d703eb235a1
leoncan122 <leon.can122@gmail.com>
2023-05-03 18:36:39 +0200

    Update links

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,22 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="py-3 px-5 md:px-7 lg:px-24 md:py-5">
       <div className="flex  justify-start gap-5 flex-row md:justify-between md:items-center">
         <div className="flex gap-5 items-start md:items-center">
           <img src="/API_symbol.svg" alt="" />
-          <a href="/">
+          <Link href="/">
             <h1 className="font-bold  max-[600px]:text-xl">FACT WIZARD</h1>
-          </a>
+          </Link>
         </div>
         <div className="flex gap-5 items-start md:items-center">
           <img src="/API_symbol.svg" alt="" />
-          <a href="/apitos-project">
+          <Link href="/apitos-project">
             <h1 className="font-bold max-[600px]:text-xl ">
               APIToS <span className="font-medium">Project</span>
             </h1>
-          </a>
+          </Link>
         </div>
       </div>
     </header>
