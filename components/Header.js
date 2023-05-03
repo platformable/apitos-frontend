6975163ac54d249dc2cfaebb5722de0da3346f83
leoncan122 <leon.can122@gmail.com>
2023-05-03 16:26:33 +0200

    Fix ilumination

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,17 @@
 const Header = () => {
 
 return (
-    <header className="py-3 px-5 md:px-7 lg:px-24">
+    <header className="py-3 px-5 md:px-7 lg:px-24 md:py-5">
         <div className="flex  justify-start gap-5 flex-row md:justify-between md:items-center">
           <div className="flex gap-5 items-start md:items-center">
             <img src="/API_symbol.svg" alt="" />
-            <h1 className="font-bold  ">FACT WIZARD</h1>
+            <h1 className="font-bold  max-[600px]:text-xl">FACT WIZARD</h1>
           </div>
           <div className="flex gap-5 items-start md:items-center">
             <img src="/API_symbol.svg" alt="" />
-            <h1 className="font-bold ">APIToS <span className="font-medium">Project</span></h1>
+           <a href="/apitos-project">
+           <h1 className="font-bold max-[600px]:text-xl ">APIToS <span className="font-medium">Project</span></h1>
+           </a>
           </div>
         </div>
       </header>
--- pages/apitos-project.js
+++ pages/apitos-project.js
@@ -1,29 +1,29 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-export default function Project() {
+export default function Project({data}) {
   return (
     <main>
       <Header />
-      <section className="bg-extralight-violet flex gap-10 items-center justify-center pt-20 pb-32">
-        <div className="px-5 md:px-7 lg:px-24 flex flex-col md:grid md:grid-flow-col gap-20  text-justify">
+      <section className="bg-extralight-violet flex gap-10 items-center justify-center pt-10 md:pb-20 pt-32">
+        <div className="px-5 md:px-7 lg:px-24 flex flex-col md:grid md:grid-flow-col gap-20 pb-10 text-justify">
           <article className="flex flex-col items-start justify-start gap-5">
-            <h2 className="font-bold mb-10">APIToS</h2>
-            <p className="font-bold">
+            <h2 className="font-bold md:mb-10">APIToS</h2>
+            <h4 className="font-medium ">
               The APIToS project was made possible by the Critical Digital
               Infrastructure Grant, the Ford Foundation, the Sloan Foundation,
               and support from the Mozilla, Omidyar and Open Society
               Foundations.
-            </p>
-            <p className="font-bold">
+            </h4>
+            <h4 className="font-medium ">
               Many thanks to the funders and the digital infrastructure grantees
               for their support and the digital exchange all along the different
               phases of the project.
-            </p>
+            </h4>
           </article>
           <div className="flex flex-col justify-center items-center gap-7">
             <img src="/cover_report.png" width={300} height={200}/>
-            <a href="#" className="font-medium underline">
+            <a href="#" className="font-medium underline max-lg:text-2xl">
               Download our report
             </a>
           </div>
@@ -34,56 +34,56 @@ export default function Project() {
           About the API Terms of Service (APIToS) project
         </h2>
         <article className="grid gap-5">
-          <div className="flex flex-col md:flex-row gap-20 rounded-xl border-white border-2 shadow py-7 px-10">
-            <img src="/icon_1_about.svg" alt="" />
-            <p className="font-mediumt text-white">
+          <div className="flex flex-col max-sm:items-center md:flex-row gap-10 md:gap-20 rounded-xl border-white border-2 shadow py-7 px-10">
+            <img src="/icon_1_about.svg" alt="" className="w-32"/>
+            <h5 className="font-mediumt text-white ">
               We intend to build a framework making API Terms of Service easily
               understandable and shareable, for a saner and safer digital
               infrastructure.
-            </p>
+            </h5>
           </div>
-          <div className="flex flex-col md:flex-row gap-20 rounded-xl border-white border-2 shadow py-7 px-10">
-            <p className="font-mediumt text-white">
+          <div className="flex flex-col max-sm:items-center md:flex-row gap-10 md:gap-20 rounded-xl border-white border-2 shadow py-7 px-10">
+            <h5 className="font-mediumt text-white">
               By conducting interviews, surveys and focus group with API users,
               providers and other stakeholders, by using a legal design approach
               and prototype testing, we have developed a model collaboratively
               to define a way forward for more standardized, accessible
               approaches to documenting and enforcing API Terms of Service.
-            </p>
+            </h5>
             <img src="/icon_2_about.svg" alt="" />
           </div>
           <div className="rounded-xl border-white border-2 shadow py-7 px-10">
-            <p className="font-mediumt text-white">
+            <h5 className="font-mediumt text-white ">
               Our research has clarified the frictions in the user experience of
               API, the power imbalance and inequalities between API producers
               and consumers explored the diversity of situations and people
               involved in the API ecosystem.
-            </p>
+            </h5>
             <br />
-            <p className="font-mediumt text-white">
+            <h5 className="font-mediumt text-white ">
               Moreover, this research designed a framework for healthier API
               ecosystems called FACT (FAIR API Commitment to Trust) related to a
               FACT license describing the mutual commitments between API
               Providers and Users.
-            </p>
+            </h5>
             <br />
 
-            <p className="font-mediumt text-white">
+            <h5 className="font-mediumt text-white ">
               Particular attention has been dedicated to the creation of
               resources that are easily readable and understandable for a wide
               audience, thus contributing to the establishment of a trustworthy
               and sustainable framework that can be easily implemented for the
               essential digital infrastructures that APIs represent.
-            </p>
+            </h5>
           </div>
         </article>
       </section>
       <section className="px-5 md:px-7 lg:px-24 py-16">
         <h2 className="mb-10">The APIToS Project Team</h2>
         <article className="grid gap-5">
-          <div className="flex border-2 border-yellow-400 rounded py-3 divide-x-2 divide-violet-500">
+          <div className="flex flex-col items-center md:items-start  gap-7 md:gap-0 md:flex-row border-2 border-yellow-400 rounded py-3 md:divide-x-2  md:divide-violet-500">
             <img className="px-5" src="Medhi_pic.png" />
-            <div className="px-3">
+            <div className="px-3 px-3 flex flex-col h-full items-center md:items-start md:items-start gap-5 text-justify">
               <h4>Mehdi Medjaoui</h4>
               <small>
                 is the founder of the worldwide apidays conferences series,
@@ -106,9 +106,9 @@ export default function Project() {
               </small>
             </div>
           </div>
-          <div className="flex border-2 border-yellow-400 rounded py-3 divide-x-2 divide-violet-500">
+          <div className="flex flex-col items-center md:items-start gap-7 md:gap-0 md:flex-row border-2 border-yellow-400 rounded py-3 md:divide-x-2  md:divide-violet-500">
             <img className="px-5" src="/Celya_pic.png" />
-            <div className="px-3">
+            <div className="px-3 px-3 flex flex-col h-full items-center md:items-start gap-5 text-justify">
               <h4>Célya Gruson-Daniel</h4>
               <small>
                 is a research-practitioner and consultant at inno3, a consulting
@@ -127,9 +127,9 @@ export default function Project() {
               </small>
             </div>
           </div>
-          <div className="flex border-2 border-yellow-400 rounded py-3 divide-x-2 divide-violet-500">
+          <div className="flex flex-col items-center md:items-start gap-7 md:gap-0 md:flex-row border-2 border-yellow-400 rounded py-3 md:divide-x-2  md:divide-violet-500">
             <img className="px-5" src="Benjamin_pic.png" />
-            <div className="px-3">
+            <div className="px-3 px-3 flex flex-col h-full items-center md:items-start gap-5 text-justify">
               <h4>Benjamin Jean</h4>
               <small>
                 is currently the inno3 company’s CEO, an open innovation
@@ -146,9 +146,9 @@ export default function Project() {
               </small>
             </div>
           </div>
-          <div className="flex border-2 border-yellow-400 rounded py-3 divide-x-2 divide-violet-500">
-            <img className="px-5" src="/Mark_pic.png" />
-            <div className="px-3">
+          <div className="flex flex-col gap-7 items-center md:items-start md:gap-0 md:flex-row border-2 border-yellow-400 rounded py-3 md:divide-x-2  md:divide-violet-500">
+            <img className="px-5 " src="/Mark_pic.png"/>
+            <div className="px-3 flex flex-col items-center h-full md:items-start justify-start gap-5 text-justify">
               <h4>Mark Boyd</h4>
               <small>
                 is an API industry analyst and writer. Mark is the founder of
@@ -163,13 +163,28 @@ export default function Project() {
             </div>
           </div>
         </article>
-        <h4 className="my-10">
+        <h5 className="my-10 italic text-opacity-80">
         We would like to thank all those who participated in this project
         through our interviews, questionnaire responses, and/or participation in
         our workshop on “New models for APITerms of Service”.
-      </h4>
+      </h5>
       </section>
-      <Footer />
+      <Footer footerText={data.footerText}/>
     </main>
   );
+}
+
+
+export async function getServerSideProps(ctx) {
+  
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/apito?fields[0]=footerText`
+  );
+  const data = await res.json();
+  console.log(data)
+  return {
+    props: {
+      data: data.data.attributes
+    },
+  };
 }
