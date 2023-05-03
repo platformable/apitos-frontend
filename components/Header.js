45e642c250d8f6b8f292ec872d3f5e48d55fa3d0
leoncan122 <leon.can122@gmail.com>
2023-05-03 18:31:13 +0200

    add link

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,23 @@
 const Header = () => {
-
-return (
+  return (
     <header className="py-3 px-5 md:px-7 lg:px-24 md:py-5">
-        <div className="flex  justify-start gap-5 flex-row md:justify-between md:items-center">
-          <div className="flex gap-5 items-start md:items-center">
-            <img src="/API_symbol.svg" alt="" />
+      <div className="flex  justify-start gap-5 flex-row md:justify-between md:items-center">
+        <div className="flex gap-5 items-start md:items-center">
+          <img src="/API_symbol.svg" alt="" />
+          <a href="/">
             <h1 className="font-bold  max-[600px]:text-xl">FACT WIZARD</h1>
-          </div>
-          <div className="flex gap-5 items-start md:items-center">
-            <img src="/API_symbol.svg" alt="" />
-           <a href="/apitos-project">
-           <h1 className="font-bold max-[600px]:text-xl ">APIToS <span className="font-medium">Project</span></h1>
-           </a>
-          </div>
+          </a>
         </div>
-      </header>
-)
-
-}
+        <div className="flex gap-5 items-start md:items-center">
+          <img src="/API_symbol.svg" alt="" />
+          <a href="/apitos-project">
+            <h1 className="font-bold max-[600px]:text-xl ">
+              APIToS <span className="font-medium">Project</span>
+            </h1>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Header;
