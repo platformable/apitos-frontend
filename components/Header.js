2c4cc8995c1322108838309275dfefc21f997d8e
Alexei Garban <garban.valdeon@gmail.com>
2023-05-09 17:54:58 +0200

    update links order

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,19 @@ const Header = () => {
   return (
     <header className="py-3 px-5 md:px-7 lg:px-24 md:py-5">
       <div className="flex  justify-start gap-5 flex-row md:justify-between md:items-center">
-        <div className="flex gap-5 items-start md:items-center">
-          <img src="/API_symbol.svg" alt="" />
-          <Link href="/wizard">
-            <h1 className="font-bold  max-[600px]:text-xl">FACT WIZARD</h1>
-          </Link>
-        </div>
+       
         <div className="flex gap-5 items-start md:items-center">
           <img src="/API_symbol.svg" alt="" />
           <Link href="/">
             <h1 className="font-bold max-[600px]:text-xl ">
               APIToS <span className="font-medium">Project</span>
             </h1>
+          </Link>
+        </div>
+        <div className="flex gap-5 items-start md:items-center">
+          <img src="/API_symbol.svg" alt="" />
+          <Link href="/wizard">
+            <h1 className="font-bold  max-[600px]:text-xl">FACT WIZARD</h1>
           </Link>
         </div>
       </div>
